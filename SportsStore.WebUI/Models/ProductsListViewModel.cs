9b200554d8a9d071f152f7c372f0e36d90cf08c6
Michal Rownicki <Michal.Rownicki@unit4.com>
2019-07-25 05:56:34 +0200

    Test commit from VS

--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace SportsStore.WebUI.Models
 {
+
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
